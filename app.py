e65f86672cfac5ad609b013ab4883485883e7731
Hiye Shin <hiyeshin@gmail.com>
2013-01-30 13:16:57 -0500

    basic structure redesigned

--- app.py
+++ app.py
@@ -11,10 +11,6 @@ from werkzeug import secure_filename
 
 from flask.ext.mongoengine import mongoengine
 
-import models
-
-import boto
-
 # Below is used to use file such as images.
 import StringIO
 
@@ -34,91 +30,9 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def index():
-    
-    photo_upload_form = models.photo_upload_form(request.form)
-
-    if request.method == "POST" and photo_upload_form.validate():
-
-    	uploaded_file = request.files['fileupload']
-    	app.logger.info("file uploaded")
-
-
-    	# 1. Generate a file name
-    	# 2. Connect to S3
-    	# 3. Get the s3 bucket and put the file
-    	# 4. After saving to s3, and then save data to database
-    	# 5. maybe connect to application.js to make it as a texture?
-
-    	if uploaded_file and allowed_file(uploaded_file.filename):
-
-    		now = datetime.datetime.now()
-    		filename = now.strftime('%Y%m%d%H%M%s') + "-" + secure_filename(uploaded_file.filename)
-            # filename = now.strftime('%Y%m%d%H%M%s') + "-" + secure_filename(uploaded_file.filename)
-    		#connect to s3
-    		s3conn = boto.connect_s3(os.environ.get('AWS_ACCESS_KEY_ID'),os.environ.get('AWS_SECRET_ACCESS_KEY'))
-
-    		# open s3 bucket, and create new Key/file
-    		# set the mimetype, content and access control
-    		b = s3conn.get_bucket(os.environ.get('AWS_BUCKET')) # bucket name defined in .env
-    		k = b.new_key(b)
-    		k.key = filename
-    		k.set_metadata("Content-Type", uploaded_file.mimetype)
-    		k.set_contents_from_string(uploaded_file.stream.read())
-    		k.make_public()
-
-    		# save information to MONGO database
-    		# which something actually save to s3
-
-    		if k and k.size > 0:
-
-    			submitted_image = models.Image()
-    			submitted_image.filename = filename # same filename of s3 bucket file
-                
-    			submitted_image.save()
-
-    		return redirect('/')
-
-    	else:
-    		return "There was an error" + uploaded_file.filename
-
-
-    else:
-    	#get existing images. maybe I'll change my mind later
-    	images = models.Image.objects.order_by('-timestamp')
-        latest_image = images[0]
-
-    	templateData = {
-    		'images' : images,
-    		'form' : photo_upload_form
-    	}
-
-        
-
-    	return render_template("main.html", **templateData)
-
-
-@app.route('/delete/<imageid>')
-def delete_image(imageid):
-
-	image = models.Image.objects.get(id=imageid)
-	if image:
-		s3conn = boto.connect_s3(os.environ.get('AWS_ACCESS_KEY_ID'), os.environ.get('AWS_SECRET_ACCESS_KEY'))
-
-		bucket = s3conn.get_bucket(os.environ.get('AWS_BUCKET'))
-		k = bucket.new_key(bucket)
-		k.key = image.filename
-		bucket.delete_key(k)
-
-		#delete from Mongo
-		image.delete()
-
-		return redirect('/')
-
-
-	else:
-		return "Unable to find requested image in database."
+    	return render_template("main.html")
 
 
 @app.route("/test")
---
+++ static/scripts/gotham.js
@@ -0,0 +1,121 @@
+$(document).ready(function(){
+	//using Modernizr to detect the devices.
+	//Touch devices don't support autoplay and may have less bandwidth
+	//and in this case, I will show the poster images instead of video.
+	var screenIndex = 1,
+		numScreens = $('.screen').length,
+		isTransitioning = false,
+		transitionDur = 1000,
+		BV,
+		videoPlayer,
+		isTouch = Modernizr.touch,
+		$bigImage = $('.big-image'),
+		$window = $(window);
+
+
+	if(!isTouch){
+		//initialize BigVideo
+		BV = new $.BigVideo({forceAutoplay: isTouch});
+		BV.init();
+		showVideo();
+
+		BV.getPlayer().addEvent('loadeddata', function(){
+			onVideoLoaded();
+		});
+
+		//adjust image positioning so it lines up with video.
+		//it's the thing I will do later
+		$bigImage
+			.css('position', 'relative')
+			.imagesLoaded( adjustImagePositioning );
+		// on window resize
+		$window.on('resize', adjustImagePositioning );
+	}
+
+	//next button click goes to next div
+	$('#next-btn').on('click', function(e){
+		e.preventDefault();
+		if( !isTransitioning ){
+			next();
+		}
+	});
+
+
+	function showVideo(){
+		BV.show($('#screen-'+screenIndex).attr('data-video'), {ambient:true} );
+	}
+
+
+	function next(){
+		isTransitioning = true;
+
+		if( screenIndex === numSceens ){
+			$bigImage.css('opacity', 1);
+			screenIndex = 1;
+		} else {
+			screenIndex++;
+		}
+
+		if(!isTouch){
+			$('#big-video-wrap').transit({'left':'-100%'}, transitionDur )
+		}
+
+		( Modernizr.csstransitions )?
+		$('.wrapper').transit(
+			{'left':'-'+(100*(screenIndex-1))+'%'},
+			transitionDur,
+			onTransitionComplete ):
+		onTransitionComplete();
+	}
+
+
+	function onVideoLoaded(){
+		$('#screen-'+screenIndex).find( '.big-image' ).transit({'opacity':0}, 500 )
+	}
+
+
+	function onTransitionComplete(){
+		isTransitioning = false;
+		if(!isTouch){
+			$('#big-video-wrap').css('left', 0 );
+			showVideo();
+		}
+	}
+
+
+	function adjustImagePositioning(){
+		$bigImage.each( function(){
+			var $img = $( this ),
+			img = new Image();
+
+		img.src = $img.attr('src');
+
+		var windowWidth = $window.width(),
+			windowHeight = $window.height(),
+			r_w = windowHeight / windowWidth,
+			i_w = img.width,
+			i_h = img.height,
+			r_i = i_h / i_w,
+			new_w, new_h, new_left, new_top;
+
+		if( r_w > r_i ){
+			new_h = windowHeight;
+			new_w = windowHeight / r_i;
+		}
+		else {
+			new_h = windowWidth * r_i;
+			new_w = windowWidth;
+		}
+
+		$img.css({
+			width: new_w,
+			height: new_h,
+			left: ( windowWIdth - new_w ) / 2,
+			top: ( windowHeight - new_h ) / 2
+		})
+		});
+	}
+
+
+
+});
